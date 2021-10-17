ce3b831600d2ada77ad3d0d83d88f49ce0144583
Gaetan Redin <gaetan.redin@cbp-group.com>
2021-10-17 15:09:07 +0200

    feature(storybook): config
    Closes 10

--- libs/atoms/buttons/src/lib/button/button.stories.ts
+++ libs/atoms/buttons/src/lib/button/button.stories.ts
@@ -1,4 +1,3 @@
-import { CommonModule } from '@angular/common';
 import { Meta, Story } from '@storybook/angular';
 import { ButtonDirective } from './button.directive';
 
@@ -8,12 +7,26 @@ export default {
   parameters: {
     jest: ['button.directive.spec.ts'],
   },
+  argTypes: {
+    color: {
+      options: ['primary', 'accent', 'warn'],
+      control: { type: 'radio' },
+    },
+    size: {
+      options: ['small', 'medium', 'large'],
+      control: { type: 'radio' },
+    },
+  },
 } as Meta<ButtonDirective>;
 
 const Template: Story<ButtonDirective> = (args: ButtonDirective) => ({
   props: args,
-  moduleMetadata: { declarations: [ButtonDirective], imports: [CommonModule] },
-  template: `<button groButton [size]="size" [outlined]="outlined" [color]="color">Click</button>`,
+  template: `<button groButton [size]='size' [outlined]='outlined' [color]='color'>Click</button>`,
 });
 
 export const Default = Template.bind({});
+Default.args = {
+  size: 'medium',
+  outlined: false,
+  color: 'primary',
+};
