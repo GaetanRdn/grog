5841e8f3da0967c68265314ee710728b1de6ac98
Gaetan Redin <gaetan.redin@cbp-group.com>
2022-02-05 13:38:09 +0100

    fix(Storybook): fix run
    Closes #22

--- .storybook/main.js
+++ .storybook/main.js
@@ -16,17 +16,6 @@ module.exports = {
     builder: 'webpack5',
   },
   webpackFinal: (config) => {
-    // Workaround for @storybook/addon-jest on Webpack 5
-    config.resolve = {
-      ...config.resolve,
-      alias: {
-        ...config.resolve.alias,
-        path: require.resolve('path-browserify'),
-        crypto: require.resolve('crypto-browserify'),
-        stream: require.resolve('stream-browserify'),
-      },
-    };
-
     return config;
   },
 };
