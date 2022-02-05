e9c8687978b3db82ff402d3c88d0f8d63a959464
GRdn <44368839+GaetanRdn@users.noreply.github.com>
2022-02-05 14:03:19 +0100

    22 nx upgrade (#29)
    * chore(Upgrade): apply convert-to-nx-project
    
    Closes #22
    
    * chore(Upgrade): apply migrations
    
    Closes #22
    
    * chore(Upgrade): apply migrations
    
    Closes #22
    
    * fix(Storybook): fix run
    
    Closes #22
    
    * fix(Storybook): install eslint-plugin-storybook
    
    Closes #22
    
    * fix(Storybook): add docs
    
    Closes #22
    
    * 0.1.0
    
    * CI(node): update version
    
    Closes #22
    
    Co-authored-by: Gaetan Redin <gaetan.redin@cbp-group.com>

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,7 @@
 module.exports = {
   stories: [],
   addons: [
+    '@storybook/addon-docs',
     '@storybook/addon-essentials',
     '@storybook/addon-jest',
     {
@@ -16,17 +17,6 @@ module.exports = {
     builder: 'webpack5',
   },
   webpackFinal: (config) => {
-    // Workaround for @storybook/addon-jest on Webpack 5
-    config.resolve = {
-      ...config.resolve,
-      alias: {
-        ...config.resolve.alias,
-        path: require.resolve('path-browserify'),
-        crypto: require.resolve('crypto-browserify'),
-        stream: require.resolve('stream-browserify'),
-      },
-    };
-
     return config;
   },
 };
