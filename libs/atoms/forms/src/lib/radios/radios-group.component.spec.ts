fbe402653b2a04aa81e6901afc1e783034687134
Gaetan Redin <gaetan.redin@cbp-group.com>
2022-03-13 18:18:50 +0100

    feature(radios): create radios buttons
    Closes #7

--- libs/atoms/forms/src/lib/radios/radios-group.component.spec.ts
+++ libs/atoms/forms/src/lib/radios/radios-group.component.spec.ts
@@ -6,55 +6,83 @@ import { RadiosGroupComponent } from './radios-group.component';
 import { RadiosModule } from './radios.module';
 
 describe('RadiosGroupComponent', () => {
-  let templateLookup: TemplateLookup<HostComponent>;
-
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [HostComponent],
+      declarations: [HostComponent, HostWithoutEqualsFnComponent],
       imports: [RadiosModule],
     });
-
-    templateLookup = new TemplateLookup(TestBed.createComponent(HostComponent));
-    jest.spyOn(templateLookup.hostComponent, 'valueChange');
   });
 
-  test('should create', () => {
-    templateLookup.detectChanges();
+  describe('Complex Object', () => {
+    let templateLookup: TemplateLookup<HostComponent>;
 
-    expect(templateLookup.firstChildElement).toMatchSnapshot();
+    beforeEach(() => {
+      templateLookup = new TemplateLookup(TestBed.createComponent(HostComponent));
+      jest.spyOn(templateLookup.hostComponent, 'valueChange');
+    });
+
+    test('should create', () => {
+      templateLookup.detectChanges();
+
+      expect(templateLookup.firstChildElement).toMatchSnapshot();
+    });
+
+    test('should create with initial value', () => {
+      templateLookup.hostComponent.value = { id: 2, name: 'no' };
+
+      templateLookup.detectChanges();
+
+      expect(templateLookup.firstChildElement).toMatchSnapshot();
+      expect(templateLookup.queryAll<HTMLInputElement>('input')[1].checked).toBe(true);
+    });
+
+    test('should set value from checked', fakeAsync(() => {
+      templateLookup.hostComponent.checked = 1;
+
+      templateLookup.detectChanges();
+      tick(100);
+
+      expect(templateLookup.firstChildElement).toMatchSnapshot();
+      expect(templateLookup.getComponent(RadiosGroupComponent).value).toEqual({ id: 1, name: 'yes' });
+      expect(templateLookup.hostComponent.valueChange).toBeCalledWith({ id: 1, name: 'yes' });
+    }));
+
+    test('should set value when check', fakeAsync(() => {
+      templateLookup.detectChanges();
+
+      templateLookup.queryAll<HTMLInputElement>('input')[0].click();
+      templateLookup.detectChanges();
+      tick(100);
+
+      expect(templateLookup.firstChildElement).toMatchSnapshot();
+      expect(templateLookup.getComponent(RadiosGroupComponent).value).toEqual({ id: 1, name: 'yes' });
+      expect(templateLookup.hostComponent.valueChange).toBeCalledWith({ id: 1, name: 'yes' });
+    }));
   });
 
-  test('should create with initial value', () => {
-    templateLookup.hostComponent.value = { id: 2, name: 'no' };
+  describe('Without EqualsFn', () => {
+    let templateLookup: TemplateLookup<HostWithoutEqualsFnComponent>;
 
-    templateLookup.detectChanges();
+    beforeEach(() => {
+      templateLookup = new TemplateLookup(TestBed.createComponent(HostWithoutEqualsFnComponent));
+      templateLookup.detectChanges();
+    });
 
-    expect(templateLookup.firstChildElement).toMatchSnapshot();
-    expect(templateLookup.queryAll<HTMLInputElement>('input')[1].checked).toBe(true);
+    test('should create', fakeAsync(() => {
+      // WHEN
+      templateLookup.queryAll<HTMLInputElement>('input')[0].click();
+      templateLookup.detectChanges();
+      tick(100);
+
+      // THEN
+      expect(templateLookup.firstChildElement).toMatchSnapshot();
+      expect(
+        templateLookup
+          .getComponent(RadiosGroupComponent)
+          .equalsFn(templateLookup.getComponent(RadiosGroupComponent).value, 1)
+      ).toBe(true);
+    }));
   });
-
-  test('should set value from checked', fakeAsync(() => {
-    templateLookup.hostComponent.checked = 1;
-
-    templateLookup.detectChanges();
-    tick(100);
-
-    expect(templateLookup.firstChildElement).toMatchSnapshot();
-    expect(templateLookup.getComponent(RadiosGroupComponent).value).toEqual({ id: 1, name: 'yes' });
-    expect(templateLookup.hostComponent.valueChange).toBeCalledWith({ id: 1, name: 'yes' });
-  }));
-
-  test('should set value when check', fakeAsync(() => {
-    templateLookup.detectChanges();
-
-    templateLookup.queryAll<HTMLInputElement>('input')[0].click();
-    templateLookup.detectChanges();
-    tick(100);
-
-    expect(templateLookup.firstChildElement).toMatchSnapshot();
-    expect(templateLookup.getComponent(RadiosGroupComponent).value).toEqual({ id: 1, name: 'yes' });
-    expect(templateLookup.hostComponent.valueChange).toBeCalledWith({ id: 1, name: 'yes' });
-  }));
 });
 
 @Component({
@@ -82,3 +110,11 @@ class HostComponent {
     this.value = val;
   }
 }
+
+@Component({
+  template: ` <gro-radios-group name="yesOrNo">
+    <gro-radio [value]="1">Yes</gro-radio>
+    <gro-radio [value]="2">No</gro-radio>
+  </gro-radios-group>`,
+})
+class HostWithoutEqualsFnComponent {}
